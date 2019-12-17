2833cec0acf44736a574601d53e3cf9db1a5685f
Bitbucket Pipeline <admin@etherisk.com>
2019-12-17 22:20:55 +0300

    Add support of SSL on Load Balancer

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -141,3 +141,58 @@ resource "aws_route_table_association" "rta_subnet_private_isolated" {
   subnet_id      = aws_subnet.subnet_private_isolated.id
   route_table_id = aws_route_table.rtb_public.id
 }
+
+resource "aws_network_acl" "private_subnet_acl" {
+  vpc_id     = aws_vpc.test_main.id
+  subnet_ids = [aws_subnet.subnet_private.id]
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 200
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 65535
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 300
+    action     = "allow"
+    cidr_block = var.cidr_subnet
+    from_port  = 22
+    to_port    = 22
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 400
+    action     = "allow"
+    cidr_block = var.cidr_subnet
+    from_port  = 8080
+    to_port    = 8080
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 500
+    action     = "allow"
+    cidr_block = var.cidr_subnet_public_az
+    from_port  = 8080
+    to_port    = 8080
+  }
+
+  tags = {
+    "Name"        = "${var.environment_tag}-private-acl",
+    "Environment" = var.environment_tag
+  }
+}
